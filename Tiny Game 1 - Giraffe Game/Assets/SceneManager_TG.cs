48048c87071c137536b3c11b52990feb51d242f8
HitoriMusuko <HitoriMusuko@users.noreply.github.com>
2019-10-02 19:05:08 +0100

    Scene Manager Script
    The scene manager script has been added to the GitHub. It should be easier to use now
    
    -Liam

---
+++ Tiny Game 1 - Giraffe Game/Assets/SceneManager_TG.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager_TG : MonoBehaviour
+{
+    public string SceneName;
+
+    public void OnMouseClick()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+
+}
